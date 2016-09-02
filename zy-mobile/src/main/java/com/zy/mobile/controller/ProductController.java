314e858e792387de50d4f9a2b3f2fa231559d47b
husky <husky@husky-PC>
2016-09-02 13:54:58 +0800

    1

--- zy-mobile/src/main/java/com/zy/mobile/controller/ProductController.java
+++ zy-mobile/src/main/java/com/zy/mobile/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.zy.mobile.controller;
 
+import static com.zy.common.util.ValidateUtils.validate;
+import static com.zy.common.util.ValidateUtils.NOT_NULL;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.zy.component.ProductComponent;
 import com.zy.entity.mal.Product;
+import com.zy.model.query.ProductQueryModel;
 import com.zy.service.ProductService;
 
 @RequestMapping("/product")
@@ -17,9 +24,18 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
+	@Autowired
+	private ProductComponent productComponent;
+	
 	@RequestMapping
 	public String list(boolean isFirst, Model model) {
 		
+		ProductQueryModel productQueryModel = new ProductQueryModel();
+		productQueryModel.setIsOnEQ(true);
+		List<Product> products = productService.findAll(productQueryModel);
+		model.addAttribute("products", products.stream().map(v -> {
+			return productComponent.buildListVo(v);
+		}));
 		if(isFirst) {
 			return "product/productListAgent";
 		} else {
@@ -27,11 +43,12 @@ public class ProductController {
 		}
 	}
 
-
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String detail(@PathVariable Long id, Model model) {
 		Product product = productService.findOne(id);
-		
+		validate(product, NOT_NULL, "product id" + id + " not found");
+		validate(product.getIsOn(), v -> true, "product not on");
+		model.addAttribute("product", productComponent.buildDetailVo(product));
 		return "product/productDetail";
 	}
 
