9863ba467a6814672d198393e7954db4248452e2
husky <husky@husky-PC>
2016-09-08 17:48:28 +0800

    1

--- zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterReportController.java
+++ zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterReportController.java
@@ -5,16 +5,24 @@ import static com.zy.common.util.ValidateUtils.validate;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.zy.common.model.query.Page;
 import com.zy.common.model.query.PageBuilder;
+import com.zy.common.model.result.Result;
 import com.zy.common.model.result.ResultBuilder;
 import com.zy.component.ReportComponent;
 import com.zy.entity.act.Report;
@@ -35,11 +43,27 @@ public class UcenterReportController {
 
 	@RequestMapping()
 	public String list(Principal principal, Model model) {
-		Page<Report> page = reportService.findPage(ReportQueryModel.builder().userIdEQ(principal.getUserId()).build());
+		Page<Report> page = reportService.findPage(ReportQueryModel.builder().userIdEQ(principal.getUserId()).pageNumber(0).pageSize(2).build());
 		model.addAttribute("page", PageBuilder.copyAndConvert(page, reportComponent::buildVo));
+		model.addAttribute("timeLT", DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
 		return "ucenter/report/reportList";
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public Result<?> product(Model model, Date timeLT, @RequestParam(required = true) Integer pageNumber, Principal principal) {
+		if (timeLT == null) {
+			timeLT = new Date();
+		}
+		
+		Map<String, Object> map = new HashMap<>();
+		Page<Report> page = reportService.findPage(ReportQueryModel.builder().userIdEQ(principal.getUserId()).build());
+		map.put("page", PageBuilder.copyAndConvert(page, reportComponent::buildVo));
+		map.put("timeLT", DateFormatUtils.format(timeLT, "yyyy-MM-dd HH:mm:ss"));
+			
+		return ResultBuilder.result(map);
+	}
+	
 	@RequestMapping(value = "create", method = GET)
 	public String create(Model model) {
 
