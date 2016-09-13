86da3e1a53fe99ca5c6746f5452db42ea99d4310
Fred <fred_fuly@126.com>
2016-09-13 13:39:55 +0800

    !!

--- zy-component/src/main/java/com/zy/component/DepositComponent.java
+++ zy-component/src/main/java/com/zy/component/DepositComponent.java
@@ -5,6 +5,9 @@ import com.zy.entity.fnc.Deposit;
 import com.zy.entity.usr.User;
 import com.zy.util.VoHelper;
 import com.zy.vo.DepositAdminVo;
+
+import static com.zy.util.GcUtils.getThumbnail;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +22,7 @@ public class DepositComponent {
 		BeanUtils.copyProperties(deposit, depositAdminVo);
 		User user = cacheComponent.getUser(deposit.getUserId());
 		depositAdminVo.setUser(VoHelper.buildUserAdminSimpleVo(user));
+		depositAdminVo.setOfflineImageThumbnail(getThumbnail(deposit.getOfflineImage()));
 		return depositAdminVo;
 	}
 	
