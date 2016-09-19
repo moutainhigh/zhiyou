0820cd3b7806695e91c19c0b50479424f94fde96
freeman <wodeoschina1>
2016-09-19 12:31:15 +0800

    短信和消息

--- zy-consumer/src/main/java/com/zy/Main.java
+++ zy-consumer/src/main/java/com/zy/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("dubbo.application.logger", "slf4j");
-        System.setProperty("dubbo.jetty.port", "8889");
-        System.setProperty("dubbo.container", "spring,jetty");
+//        System.setProperty("dubbo.jetty.port", "8889");
+        System.setProperty("dubbo.container", "spring");
         com.alibaba.dubbo.container.Main.main(args);
     }
 
