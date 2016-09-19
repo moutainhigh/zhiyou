98e4f78975d99787c8f0ecb3d82160a0e386cf3b
Fred <fred_fuly@126.com>
2016-09-19 17:57:05 +0800

    !

--- zy-mobile/src/main/java/com/zy/mobile/controller/ControllerAdvice.java
+++ zy-mobile/src/main/java/com/zy/mobile/controller/ControllerAdvice.java
@@ -60,7 +60,7 @@ public class ControllerAdvice {
 		if (requestUrl.startsWith("/activity")) {
 			String url = request.getRequestURL().toString();
 			String queryStr = request.getQueryString();
-			/*if (principal != null) {
+			if (principal != null) {
 				Long userId = principal.getUserId();
 				if (queryStr != null){
 					url += "?" + queryStr + "&__u=" + userId;
@@ -71,11 +71,6 @@ public class ControllerAdvice {
 				if (queryStr != null){
 					url += "?" + queryStr;
 				}
-			}*/
-			if (queryStr != null){
-				url += "?" + queryStr;
-			} else {
-				url += "?" + "a=1&b=2";
 			}
 			
 			model.addAttribute("url", url);
