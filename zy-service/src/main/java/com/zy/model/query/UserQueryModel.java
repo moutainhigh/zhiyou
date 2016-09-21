b362f9518be1b7be5686348bfda08817b4797717
husky <husky@husky-PC>
2016-09-21 18:21:31 +0800

    1

--- zy-service/src/main/java/com/zy/model/query/UserQueryModel.java
+++ zy-service/src/main/java/com/zy/model/query/UserQueryModel.java
@@ -5,14 +5,16 @@ import java.util.HashSet;
 import java.io.Serializable;
 
 import io.gd.generator.api.query.Direction;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+
 import com.zy.entity.usr.User.UserType;
+
 import java.util.Date;
+
 import com.zy.entity.usr.User.UserRank;
 
 @Getter
@@ -30,6 +32,8 @@ public class UserQueryModel implements Serializable {
 
 	private UserType userTypeEQ;
 
+	private Boolean isFrozenEQ;
+	
 	private UserRank userRankEQ;
 
 	private Date registerTimeGTE;
