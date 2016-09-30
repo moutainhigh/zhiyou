3114fc80fbd6f127093977051b962c5ba5f9f804
freeman <247687009@qq.com>
2016-09-30 15:52:20 +0800

    fix error import package

--- zy-component/src/main/java/com/zy/component/LocalCacheComponent.java
+++ zy-component/src/main/java/com/zy/component/LocalCacheComponent.java
@@ -11,38 +11,66 @@ import com.zy.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 @Component
 public class LocalCacheComponent {
 
-	final Logger logger = LoggerFactory.getLogger(LocalCacheComponent.class);
+    final Logger logger = LoggerFactory.getLogger(LocalCacheComponent.class);
 
+    private Map<Long, OrderItem> orderItemMap = new ConcurrentHashMap<>();
 
-	@Autowired
-	private OrderService orderService;
+    private List<User> users = new ArrayList<>();
 
-	@Autowired
-	private OrderItemService orderItemService;
+    private List<Order> orders = new ArrayList<>();
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private OrderService orderService;
 
-	public List<User> getUsers() {
-		return userService.findAll(new UserQueryModel());
-	}
+    @Autowired
+    private OrderItemService orderItemService;
 
-	public List<Order> getOrders() {
-		return orderService.findAll(new OrderQueryModel());
-	}
+    @Autowired
+    private UserService userService;
 
-	public Map<Long, OrderItem> getOrderItemMap() {
-		return orderItemService.findAll().stream().collect(Collectors.toMap(v -> v.getOrderId() , v -> v));
-	}
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public Map<Long, OrderItem> getOrderItemMap() {
+        return orderItemMap;
+    }
+
+    public void refresh() {
+        logger.info("refresh begin...");
+        List<User> newUsers = userService.findAll(new UserQueryModel());
+        List<Order> newOrders = orderService.findAll(new OrderQueryModel());
+        Map<Long, OrderItem> newOrderItemMap = orderItemService.findAll().stream().collect(Collectors.toMap(v -> v.getOrderId(), v -> v));
+        users = newUsers;
+        orders = newOrders;
+        orderItemMap = newOrderItemMap;
+        logger.info("refresh end...");
+
+    }
+
+    @PostConstruct
+    public void init() {
+        refresh();
+    }
 
 
 }
