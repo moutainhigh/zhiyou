9e15e82f68740c6c097ad5e607c12779bc048481
husky <husky@husky-PC>
2016-10-17 14:57:08 +0800

    1

--- zy-admin/src/main/java/com/zy/admin/model/OrderReportVo.java
+++ zy-admin/src/main/java/com/zy/admin/model/OrderReportVo.java
@@ -1,6 +1,13 @@
 package com.zy.admin.model;
 
-public class OrderReportVo {
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderReportVo implements Serializable {
 	
 	private String nickname;  //服务商昵称
 	
@@ -10,59 +17,14 @@ public class OrderReportVo {
 	
 	private String parentNickname;  //直属特级昵称
 	
+	@Getter
+	@Setter
 	public static class OrderReportVoItem {
 		
 		private String timeLabel;  //时间 yyyy-MM or yyyy-MM-dd
 		
 		private String quantity;  //进货量
 
-		public String getTimeLabel() {
-			return timeLabel;
-		}
-
-		public void setTimeLabel(String timeLabel) {
-			this.timeLabel = timeLabel;
-		}
-
-		public String getQuantity() {
-			return quantity;
-		}
-
-		public void setQuantity(String quantity) {
-			this.quantity = quantity;
-		}
-		
 	}
 
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getRootName() {
-		return rootName;
-	}
-
-	public void setRootName(String rootName) {
-		this.rootName = rootName;
-	}
-
-	public String getParentNickname() {
-		return parentNickname;
-	}
-
-	public void setParentNickname(String parentNickname) {
-		this.parentNickname = parentNickname;
-	}
 }
