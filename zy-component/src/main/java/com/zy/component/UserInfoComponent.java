c3a0cc73e7b6c3cf2634661ce7634305ffbea9e8
freeman <247687009@qq.com>
2016-10-21 15:44:04 +0800

    consumer update

--- zy-component/src/main/java/com/zy/component/UserInfoComponent.java
+++ zy-component/src/main/java/com/zy/component/UserInfoComponent.java
@@ -22,104 +22,111 @@ import java.util.stream.Collectors;
 @Component
 public class UserInfoComponent {
 
-	Logger logger = LoggerFactory.getLogger(UserInfoComponent.class);
-	
-	@Autowired
-	private CacheComponent cacheComponent;
-	
-	private static final String TIME_PATTERN = "yyyy-MM-dd HH:mm";
-	
-	public UserInfoVo buildVo(UserInfo userInfo) {
-		UserInfoVo userInfoVo = new UserInfoVo();
-		BeanUtils.copyProperties(userInfo, userInfoVo);
-		
-		String idCardNumber = userInfo.getIdCardNumber();
-		userInfoVo.setIdCardNumber(StringUtils.overlay(idCardNumber, "***********", 6, idCardNumber.length()));
-		
-		String image1 = userInfo.getImage1();
-		String image2 = userInfo.getImage2();
-		
-		userInfoVo.setImage1Thumbnail(StringUtils.isBlank(image1)? null : GcUtils.getThumbnail(image1, 240, 150));
-		userInfoVo.setImage2Thumbnail(StringUtils.isBlank(image2)? null : GcUtils.getThumbnail(image2, 240, 150));
-		
-		AreaDto areaDto = cacheComponent.getAreaDto(userInfo.getAreaId());
-		if (areaDto != null) {
-			userInfoVo.setProvince(areaDto.getProvince());
-			userInfoVo.setCity(areaDto.getCity());
-			userInfoVo.setDistrict(areaDto.getDistrict());
-		} else {
-			logger.error("area id " + userInfo.getAreaId() + " is not found");
-		}
+    Logger logger = LoggerFactory.getLogger(UserInfoComponent.class);
 
-		
-		Long jobId = userInfo.getJobId();
-		if (jobId != null) {
-			Job job = cacheComponent.getJob(userInfo.getJobId());
-			if (job != null) {
-				userInfoVo.setJobName(job.getJobName());
-			}
-		}
-		
-		String tagIds = userInfo.getTagIds();
-		if (StringUtils.isNotBlank(tagIds)) {
-			List<String> tagNames = Arrays.stream(StringUtils.split(tagIds, ","))
-				.filter(v -> StringUtils.isNotBlank(v))
-				.map(v -> Long.valueOf(v))
-				.map(v -> cacheComponent.getTag(v))
-				.filter(v -> v != null)
-				.map(v -> v.getTagName())
-				.collect(Collectors.toList());
-			userInfoVo.setTagNames(tagNames);
-		}
-		
-		userInfoVo.setBirthdayLabel(GcUtils.formatDate(userInfo.getBirthday(), "yyyy-MM-dd"));
-		
-		return userInfoVo;
-	}
-	
-	public UserInfoAdminVo buildAdminVo(UserInfo userInfo) {
-		UserInfoAdminVo userInfoAdminVo = new UserInfoAdminVo();
-		BeanUtils.copyProperties(userInfo, userInfoAdminVo);
-		User user = cacheComponent.getUser(userInfo.getUserId());
-		userInfoAdminVo.setUser(VoHelper.buildUserAdminSimpleVo(user));
-		userInfoAdminVo.setImage1Thumbnail(GcUtils.getThumbnail(userInfo.getImage1(), 240, 150));
-		userInfoAdminVo.setImage2Thumbnail(GcUtils.getThumbnail(userInfo.getImage2(), 240, 150));
-		userInfoAdminVo.setAppliedTimeLabel(GcUtils.formatDate(userInfo.getAppliedTime(), TIME_PATTERN));
-		userInfoAdminVo.setConfirmedTimeLabel(GcUtils.formatDate(userInfo.getConfirmedTime(), TIME_PATTERN));
-		userInfoAdminVo.setConfirmStatusStyle(GcUtils.getConfirmStatusStyle(userInfo.getConfirmStatus()));
-		userInfoAdminVo.setBirthdayLabel(GcUtils.formatDate(userInfo.getBirthday(), "yyyy-MM-dd"));
-		
-		Long areaId = userInfo.getAreaId();
-		if(areaId != null) {
-			AreaDto areaDto = cacheComponent.getAreaDto(areaId);
-			if (areaDto != null) {
-				userInfoAdminVo.setProvince(areaDto.getProvince());
-				userInfoAdminVo.setCity(areaDto.getCity());
-				userInfoAdminVo.setDistrict(areaDto.getDistrict());
-			}
-		}
+    @Autowired
+    private CacheComponent cacheComponent;
 
-		Long jobId = userInfo.getJobId();
-		if (jobId != null) {
-			Job job = cacheComponent.getJob(jobId);
-			if (job != null) {
-				userInfoAdminVo.setJobName(job.getJobName());	
-			}
-		}
-		
-		String tagIds = userInfo.getTagIds();
-		if (StringUtils.isNotBlank(tagIds)) {
-			List<String> tagNames = Arrays.stream(StringUtils.split(tagIds, ","))
-				.filter(v -> StringUtils.isNotBlank(v))
-				.map(v -> Long.valueOf(v))
-				.map(v -> cacheComponent.getTag(v))
-				.filter(v -> v != null)
-				.map(v -> v.getTagName())
-				.collect(Collectors.toList());
-			userInfoAdminVo.setTagNames(tagNames);
-		}
-		
-		userInfoAdminVo.setBirthdayLabel(GcUtils.formatDate(userInfo.getBirthday(), "yyyy-MM-dd"));
-		return userInfoAdminVo;
-	}
+    private static final String TIME_PATTERN = "yyyy-MM-dd HH:mm";
+
+    public UserInfoVo buildVo(UserInfo userInfo) {
+
+        UserInfoVo userInfoVo = new UserInfoVo();
+        BeanUtils.copyProperties(userInfo, userInfoVo);
+
+        String idCardNumber = userInfo.getIdCardNumber();
+        userInfoVo.setIdCardNumber(StringUtils.overlay(idCardNumber, "***********", 6, idCardNumber.length()));
+
+        String image1 = userInfo.getImage1();
+        String image2 = userInfo.getImage2();
+
+        userInfoVo.setImage1Thumbnail(StringUtils.isBlank(image1) ? null : GcUtils.getThumbnail(image1, 240, 150));
+        userInfoVo.setImage2Thumbnail(StringUtils.isBlank(image2) ? null : GcUtils.getThumbnail(image2, 240, 150));
+
+        logger.error( "user info  " + userInfo );
+
+
+
+        Long areaId = userInfo.getAreaId();
+        if (areaId != null) {
+            AreaDto areaDto = cacheComponent.getAreaDto(areaId);
+            if (areaDto != null) {
+                userInfoVo.setProvince(areaDto.getProvince());
+                userInfoVo.setCity(areaDto.getCity());
+                userInfoVo.setDistrict(areaDto.getDistrict());
+            } else {
+                logger.error("area id " + areaId + " is not found");
+            }
+        }
+
+        Long jobId = userInfo.getJobId();
+        if (jobId != null) {
+            Job job = cacheComponent.getJob(userInfo.getJobId());
+            if (job != null) {
+                userInfoVo.setJobName(job.getJobName());
+            }
+        }
+
+        String tagIds = userInfo.getTagIds();
+        if (StringUtils.isNotBlank(tagIds)) {
+            List<String> tagNames = Arrays.stream(StringUtils.split(tagIds, ","))
+                    .filter(v -> StringUtils.isNotBlank(v))
+                    .map(v -> Long.valueOf(v))
+                    .map(v -> cacheComponent.getTag(v))
+                    .filter(v -> v != null)
+                    .map(v -> v.getTagName())
+                    .collect(Collectors.toList());
+            userInfoVo.setTagNames(tagNames);
+        }
+
+        userInfoVo.setBirthdayLabel(GcUtils.formatDate(userInfo.getBirthday(), "yyyy-MM-dd"));
+
+        return userInfoVo;
+    }
+
+    public UserInfoAdminVo buildAdminVo(UserInfo userInfo) {
+        UserInfoAdminVo userInfoAdminVo = new UserInfoAdminVo();
+        BeanUtils.copyProperties(userInfo, userInfoAdminVo);
+        User user = cacheComponent.getUser(userInfo.getUserId());
+        userInfoAdminVo.setUser(VoHelper.buildUserAdminSimpleVo(user));
+        userInfoAdminVo.setImage1Thumbnail(GcUtils.getThumbnail(userInfo.getImage1(), 240, 150));
+        userInfoAdminVo.setImage2Thumbnail(GcUtils.getThumbnail(userInfo.getImage2(), 240, 150));
+        userInfoAdminVo.setAppliedTimeLabel(GcUtils.formatDate(userInfo.getAppliedTime(), TIME_PATTERN));
+        userInfoAdminVo.setConfirmedTimeLabel(GcUtils.formatDate(userInfo.getConfirmedTime(), TIME_PATTERN));
+        userInfoAdminVo.setConfirmStatusStyle(GcUtils.getConfirmStatusStyle(userInfo.getConfirmStatus()));
+        userInfoAdminVo.setBirthdayLabel(GcUtils.formatDate(userInfo.getBirthday(), "yyyy-MM-dd"));
+
+        Long areaId = userInfo.getAreaId();
+        if (areaId != null) {
+            AreaDto areaDto = cacheComponent.getAreaDto(areaId);
+            if (areaDto != null) {
+                userInfoAdminVo.setProvince(areaDto.getProvince());
+                userInfoAdminVo.setCity(areaDto.getCity());
+                userInfoAdminVo.setDistrict(areaDto.getDistrict());
+            }
+        }
+
+        Long jobId = userInfo.getJobId();
+        if (jobId != null) {
+            Job job = cacheComponent.getJob(jobId);
+            if (job != null) {
+                userInfoAdminVo.setJobName(job.getJobName());
+            }
+        }
+
+        String tagIds = userInfo.getTagIds();
+        if (StringUtils.isNotBlank(tagIds)) {
+            List<String> tagNames = Arrays.stream(StringUtils.split(tagIds, ","))
+                    .filter(v -> StringUtils.isNotBlank(v))
+                    .map(v -> Long.valueOf(v))
+                    .map(v -> cacheComponent.getTag(v))
+                    .filter(v -> v != null)
+                    .map(v -> v.getTagName())
+                    .collect(Collectors.toList());
+            userInfoAdminVo.setTagNames(tagNames);
+        }
+
+        userInfoAdminVo.setBirthdayLabel(GcUtils.formatDate(userInfo.getBirthday(), "yyyy-MM-dd"));
+        return userInfoAdminVo;
+    }
 }
