1ebf1e9c6b8fbc540f97fb9843ce4b2958002b7c
husky <husky@husky-PC>
2016-11-15 17:23:45 +0800

    policy component

---
+++ zy-component/src/main/java/com/zy/component/PolicyCodeComponent.java
@@ -0,0 +1,31 @@
+package com.zy.component;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import com.zy.common.util.BeanUtils;
+import com.zy.entity.act.PolicyCode;
+import com.zy.util.GcUtils;
+import com.zy.vo.PolicyCodeAdminVo;
+
+@Component
+public class PolicyCodeComponent {
+
+	private static final String TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+	
+	public PolicyCodeAdminVo buildAdminVo(PolicyCode policyCode) {
+		PolicyCodeAdminVo policyCodeAdminVo = new PolicyCodeAdminVo();
+		BeanUtils.copyProperties(policyCode, policyCodeAdminVo);
+		
+		Date usedTime = policyCode.getUsedTime();
+		if(usedTime != null) {
+			policyCodeAdminVo.setUsedTimeLabel(GcUtils.formatDate(usedTime, TIME_PATTERN));
+		}
+		Date createdTime = policyCode.getCreatedTime();
+		if(createdTime != null) {
+			policyCodeAdminVo.setCreatedTimeLabel(GcUtils.formatDate(createdTime, TIME_PATTERN));
+		}
+		return policyCodeAdminVo;
+	}
+}
