91d32514f6bc2624315f9b0169496e3df0b3330b
freeman <247687009@qq.com>
2016-11-23 16:34:20 +0800

    修改x坐标

--- zy-service-impl/src/main/java/com/zy/ServiceBootstrap.java
+++ zy-service-impl/src/main/java/com/zy/ServiceBootstrap.java
@@ -4,14 +4,12 @@ import com.alibaba.dubbo.container.Main;
 
 public class ServiceBootstrap {
 
-	public static void main(String[] args) {
-		//start jetty server  on port 0.0.0.0:8888
-		//localhost:8888
-//		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "/Users/freeman/IdeaProjects/zy-parent/zy-service-impl/src/main/cglib");
-		System.setProperty("dubbo.jetty.port","8889");
-		System.setProperty("dubbo.application.logger","slf4j");
-		System.setProperty("dubbo.container", "spring,jetty");
-		Main.main(args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("dubbo.application.logger", "slf4j");
+        System.setProperty("container", "spring");
+        System.setProperty("dubbo.shutdown.hook", "true");
+        Main.main(args);
+    }
 
 }
