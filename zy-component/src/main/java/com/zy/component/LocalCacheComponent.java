e3c830337aac3a0ee35d1da35d5b94899f16a14a
husky <852106886@qq.com>
2017-05-03 14:26:45 +0800

    localCacheComponent 加入线程池管理

--- zy-component/src/main/java/com/zy/component/LocalCacheComponent.java
+++ zy-component/src/main/java/com/zy/component/LocalCacheComponent.java
@@ -27,30 +27,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
+
+import static com.zy.component.ThreadPool.executorService;
 
 @Component
 public class LocalCacheComponent {
 
-    final Logger logger = LoggerFactory.getLogger(LocalCacheComponent.class);
+	final Logger logger = LoggerFactory.getLogger(LocalCacheComponent.class);
 
-    private Map<Long, OrderItem> orderItemMap = new HashMap<>();
+	private Map<Long, OrderItem> orderItemMap = new HashMap<>();
 
-    private List<User> users = new ArrayList<>();
+	private List<User> users = new ArrayList<>();
 
-    private List<Order> orders = new ArrayList<>();
-    
-    private List<Payment> payments = new ArrayList<>();
-    
-    private List<Deposit> deposits = new ArrayList<>();
-    
-    private List<Withdraw> withdraws = new ArrayList<>();
-    
-    private List<Profit> profits = new ArrayList<>();
+	private List<Order> orders = new ArrayList<>();
 
-    private List<Transfer> transfers = new ArrayList<>();
-    
-    private List<Account> accounts = new ArrayList<>();
+	private List<Payment> payments = new ArrayList<>();
+
+	private List<Deposit> deposits = new ArrayList<>();
+
+	private List<Withdraw> withdraws = new ArrayList<>();
+
+	private List<Profit> profits = new ArrayList<>();
+
+	private List<Transfer> transfers = new ArrayList<>();
+
+	private List<Account> accounts = new ArrayList<>();
 
 	private Map<Long, User> userMap = new HashMap<>();
 
@@ -59,36 +64,36 @@ public class LocalCacheComponent {
 	private List<UserReportVo> userReportVos = new ArrayList<>();
 
 	private List<String> rootNames = new ArrayList<String>();
-	
+
 	private List<Area> areas = new ArrayList<>();
 
 	private Map<Long, Area> areaMap = new HashMap<>();
 
 	private List<UserUpgrade> userUpgrades = new ArrayList<>();
 
-    @Autowired
-    private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-    @Autowired
-    private PaymentService paymentService;
-    
-    @Autowired
-    private DepositService depositService;
-    
-    @Autowired
-    private WithdrawService withdrawService;
-    
-    @Autowired
-    private ProfitService profitService;
-    
-    @Autowired
-    private AccountService accountService;
-    
-    @Autowired
-    private OrderItemService orderItemService;
+	@Autowired
+	private PaymentService paymentService;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private DepositService depositService;
+
+	@Autowired
+	private WithdrawService withdrawService;
+
+	@Autowired
+	private ProfitService profitService;
+
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private OrderItemService orderItemService;
+
+	@Autowired
+	private UserService userService;
 
 	@Autowired
 	private AreaService areaService;
@@ -102,11 +107,13 @@ public class LocalCacheComponent {
 	@Autowired
 	private UserUpgradeService userUpgradeService;
 
-	public List<Area> getAreas() { return areas; }
+	public List<Area> getAreas() {
+		return areas;
+	}
 
-    public List<User> getUsers() {
-        return users;
-    }
+	public List<User> getUsers() {
+		return users;
+	}
 
 	public Map<Long, User> getUserMap() {
 		return userMap;
@@ -116,35 +123,37 @@ public class LocalCacheComponent {
 		return userInfoMap;
 	}
 
-    public List<Order> getOrders() {
-        return orders;
-    }
-    
-    public List<Payment> getPayments() {
-        return payments;
-    }
-    
-    public List<Deposit> getDeposits() {
-        return deposits;
-    }
-    
-    public List<Withdraw> getWithdraws() {
-        return withdraws;
-    }
-    
-    public List<Profit> getProfits() {
-        return profits;
-    }
+	public List<Order> getOrders() {
+		return orders;
+	}
 
-    public List<Transfer> getTransfers() { return transfers; }
-    
-    public List<Account> getAccounts() {
-        return accounts;
-    }
+	public List<Payment> getPayments() {
+		return payments;
+	}
 
-    public Map<Long, OrderItem> getOrderItemMap() {
-        return orderItemMap;
-    }
+	public List<Deposit> getDeposits() {
+		return deposits;
+	}
+
+	public List<Withdraw> getWithdraws() {
+		return withdraws;
+	}
+
+	public List<Profit> getProfits() {
+		return profits;
+	}
+
+	public List<Transfer> getTransfers() {
+		return transfers;
+	}
+
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public Map<Long, OrderItem> getOrderItemMap() {
+		return orderItemMap;
+	}
 
 	public List<UserReportVo> getuserReportVos() {
 		return userReportVos;
@@ -153,137 +162,194 @@ public class LocalCacheComponent {
 	public List<String> getRootNames() {
 		return rootNames;
 	}
-	
+
 	public List<UserUpgrade> getUserUpgrades() {
 		return userUpgrades;
 	}
 
-    public void refresh() {
-        logger.info("refresh begin...");
-	    List<Area> newAreas = areaService.findAll(new AreaQueryModel());
-	    Map<Long, Area> newAreaMap = newAreas.stream().collect(Collectors.toMap(v -> v.getId(), v -> v));
+	public void refresh() throws InterruptedException {
+		CountDownLatch downLatch = new CountDownLatch(12);
 
-        List<User> newUsers = userService.findAll(UserQueryModel.builder().userTypeEQ(User.UserType.代理).build());
-        List<String> newRootNames = newUsers.stream().filter(v -> v.getIsRoot() != null && v.getIsRoot()).map(User::getRootName).distinct().collect(Collectors.toList());
-        List<Order> newOrders = orderService.findAll(OrderQueryModel.builder().build());
-        List<Payment> newPayments = paymentService.findAll(PaymentQueryModel.builder().paymentStatusEQ(PaymentStatus.已支付).build());
-        List<Deposit> newDeposits = depositService.findAll(DepositQueryModel.builder().depositStatusEQ(DepositStatus.充值成功).build());
-        List<Withdraw> newWithdraws = withdrawService.findAll(WithdrawQueryModel.builder().withdrawStatusEQ(WithdrawStatus.提现成功).build());
-        List<Profit> newProfits = profitService.findAll(ProfitQueryModel.builder().profitStatusEQ(ProfitStatus.已发放).build());
-        List<Transfer> newTransfer = transferService.findAll(TransferQueryModel.builder().transferTypeEQ(Transfer.TransferType.U币转账).transferStatusEQ(Transfer.TransferStatus.已转账).build());
-        List<Account> newAccounts = accountService.findAll(AccountQueryModel.builder().build());
-        Map<Long, OrderItem> newOrderItemMap = orderItemService.findAll().stream().collect(Collectors.toMap(v -> v.getOrderId(), v -> v));
-	    Map<Long, User> newUserMap = newUsers.stream().collect(Collectors.toMap(v -> v.getId(), v -> v));
-	    Map<Long, UserInfo> newUserInfoMap = userInfoService.findAll(UserInfoQueryModel.builder().build()).stream().collect(Collectors.toMap(v -> v.getUserId(), v -> v));
-	    List<UserUpgrade> newUserUpgrades = userUpgradeService.findAll(UserUpgradeQueryModel.builder().build());
+		logger.info("refresh begin...");
+		List<Area> newAreas = new ArrayList<>();
+		List<User> newUsers = new ArrayList<>();
+		Map<Long, User> newUserMap = new HashMap<>();
+		Map<Long, Area> newAreaMap = new HashMap<>();
+		List<String> newRootNames = new ArrayList<>();
+		List<Order> newOrders = new ArrayList<>();
+		List<Payment> newPayments = new ArrayList<>();
+		List<Deposit> newDeposits = new ArrayList<>();
+		List<Withdraw> newWithdraws = new ArrayList<>();
+		List<Profit> newProfits = new ArrayList<>();
+		List<Transfer> newTransfer = new ArrayList<>();
+		List<Account> newAccounts = new ArrayList<>();
+		Map<Long, OrderItem> newOrderItemMap = new HashMap<>();
+		Map<Long, UserInfo> newUserInfoMap = new HashMap<>();
+		List<UserUpgrade> newUserUpgrades = new ArrayList<>();
 
-	    areas = newAreas;
-	    areaMap = newAreaMap;
-        users = newUsers;
-        orders = newOrders;
-        payments = newPayments;
-        deposits = newDeposits;
-        withdraws = newWithdraws;
-        profits = newProfits;
-        transfers = newTransfer;
-        accounts = newAccounts;
-        orderItemMap = newOrderItemMap;
-	    userMap = newUserMap;
-	    userInfoMap = newUserInfoMap;
-	    userUpgrades = newUserUpgrades;
-	    rootNames = newRootNames;
+		executorService.execute(() -> {
+			newAreas.addAll(areaService.findAll(new AreaQueryModel()));
+			newAreaMap.putAll(newAreas.stream().collect(Collectors.toMap(v -> v.getId(), v -> v)));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newUsers.addAll(userService.findAll(UserQueryModel.builder().userTypeEQ(User.UserType.代理).build()));
+			newRootNames.addAll(newUsers.stream().filter(v -> v.getIsRoot() != null && v.getIsRoot()).map(User::getRootName).distinct().collect(Collectors.toList()));
+			newUserMap.putAll(newUsers.stream().collect(Collectors.toMap(v -> v.getId(), v -> v)));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newOrders.addAll(orderService.findAll(OrderQueryModel.builder().build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newPayments.addAll(paymentService.findAll(PaymentQueryModel.builder().paymentStatusEQ(PaymentStatus.已支付).build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newDeposits.addAll(depositService.findAll(DepositQueryModel.builder().depositStatusEQ(DepositStatus.充值成功).build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newWithdraws.addAll(withdrawService.findAll(WithdrawQueryModel.builder().withdrawStatusEQ(WithdrawStatus.提现成功).build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newProfits.addAll(profitService.findAll(ProfitQueryModel.builder().profitStatusEQ(ProfitStatus.已发放).build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newTransfer.addAll(transferService.findAll(TransferQueryModel.builder().transferTypeEQ(Transfer.TransferType.U币转账).transferStatusEQ(Transfer.TransferStatus.已转账).build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newAccounts.addAll(accountService.findAll(AccountQueryModel.builder().build()));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newOrderItemMap.putAll(orderItemService.findAll().stream().collect(Collectors.toMap(v -> v.getOrderId(), v -> v)));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newUserInfoMap.putAll(userInfoService.findAll(UserInfoQueryModel.builder().build()).stream().collect(Collectors.toMap(v -> v.getUserId(), v -> v)));
+			downLatch.countDown();
+		});
+		executorService.execute(() -> {
+			newUserUpgrades.addAll(userUpgradeService.findAll(UserUpgradeQueryModel.builder().build()));
+			downLatch.countDown();
+		});
 
-	    userReportVos = newUsers.stream().map(user -> {
-		    UserReportVo userReportVo = new UserReportVo();
-		    BeanUtils.copyProperties(user, userReportVo);
+		downLatch.await();
+
+		areas = newAreas;
+		areaMap = newAreaMap;
+		users = newUsers;
+		orders = newOrders;
+		payments = newPayments;
+		deposits = newDeposits;
+		withdraws = newWithdraws;
+		profits = newProfits;
+		transfers = newTransfer;
+		accounts = newAccounts;
+		orderItemMap = newOrderItemMap;
+		userMap = newUserMap;
+		userInfoMap = newUserInfoMap;
+		userUpgrades = newUserUpgrades;
+		rootNames = newRootNames;
+
+		userReportVos = newUsers.stream().map(user -> {
+			UserReportVo userReportVo = new UserReportVo();
+			BeanUtils.copyProperties(user, userReportVo);
 			Long userId = user.getId();
-		    UserInfo userInfo = newUserInfoMap.get(userId);
-		    if (userInfo != null) {
+			UserInfo userInfo = newUserInfoMap.get(userId);
+			if (userInfo != null) {
 				Long areaId = userInfo.getAreaId();
-			    if (areaId != null) {
-				    Long districtId = areaId;
-				    Area district = areaMap.get(districtId);
-				    if (district != null && district.getAreaType() == Area.AreaType.区) {
-					    userReportVo.setDistrictId(districtId);
-					    Long cityId = district.getParentId();
-					    Area city = areaMap.get(cityId);
-					    if (city != null && city.getAreaType() == Area.AreaType.市) {
-						    userReportVo.setCityId(cityId);
-						    Long provinceId = city.getParentId();
-						    Area province = areaMap.get(provinceId);
-						    if (province != null && province.getAreaType() == Area.AreaType.省) {
-							    userReportVo.setProvinceId(provinceId);
-						    }
-					    }
+				if (areaId != null) {
+					Long districtId = areaId;
+					Area district = areaMap.get(districtId);
+					if (district != null && district.getAreaType() == Area.AreaType.区) {
+						userReportVo.setDistrictId(districtId);
+						Long cityId = district.getParentId();
+						Area city = areaMap.get(cityId);
+						if (city != null && city.getAreaType() == Area.AreaType.市) {
+							userReportVo.setCityId(cityId);
+							Long provinceId = city.getParentId();
+							Area province = areaMap.get(provinceId);
+							if (province != null && province.getAreaType() == Area.AreaType.省) {
+								userReportVo.setProvinceId(provinceId);
+							}
+						}
 
-				    }
-			    }
-		    }
+					}
+				}
+			}
 
-		    Long parentId = user.getParentId();
-		    int whileTimes = 0;
+			Long parentId = user.getParentId();
+			int whileTimes = 0;
 
-		    Long v4UserId = null;
-		    Long tmpParentId = parentId;
-		    while (tmpParentId != null) {
-			    if (whileTimes > 1000) {
-				    throw new BizException(BizCode.ERROR, "循环引用错误, user id is " + userId);
-			    }
-			    User parent = newUserMap.get(tmpParentId);
-			    if (parent == null) {
-				    throw new BizException(BizCode.ERROR, "关联了错误的parent id " + tmpParentId);
-			    }
-			    if (parent.getUserRank() == User.UserRank.V4) {
-				    v4UserId =  tmpParentId;
-				    break;
-			    }
-			    tmpParentId = parent.getParentId();
-			    whileTimes ++;
-		    }
-		    if (v4UserId != null) {
-			    User v4User = newUserMap.get(v4UserId);
-			    userReportVo.setV4UserId(v4UserId);
-			    userReportVo.setV4UserNickname(v4User.getNickname());
-		    }
+			Long v4UserId = null;
+			Long tmpParentId = parentId;
+			while (tmpParentId != null) {
+				if (whileTimes > 1000) {
+					throw new BizException(BizCode.ERROR, "循环引用错误, user id is " + userId);
+				}
+				User parent = newUserMap.get(tmpParentId);
+				if (parent == null) {
+					throw new BizException(BizCode.ERROR, "关联了错误的parent id " + tmpParentId);
+				}
+				if (parent.getUserRank() == User.UserRank.V4) {
+					v4UserId = tmpParentId;
+					break;
+				}
+				tmpParentId = parent.getParentId();
+				whileTimes++;
+			}
+			if (v4UserId != null) {
+				User v4User = newUserMap.get(v4UserId);
+				userReportVo.setV4UserId(v4UserId);
+				userReportVo.setV4UserNickname(v4User.getNickname());
+			}
 
-		    Long rootId = null;
-		    if (user.getIsRoot() != null && user.getIsRoot()) {
-			    rootId = userId;
-		    } else {
-			    tmpParentId = parentId;
-			    whileTimes = 0;
-			    while (tmpParentId != null) {
-				    if (whileTimes > 1000) {
-					    throw new BizException(BizCode.ERROR, "循环引用错误, user id is " + user.getId());
-				    }
-				    User parent =  newUserMap.get(tmpParentId);
-				    if (parent.getIsRoot() != null && parent.getIsRoot()) {
-					    rootId = tmpParentId;
-					    break;
-				    }
-				    tmpParentId = parent.getParentId();
-				    whileTimes++;
-			    }
-		    }
-		    if (rootId != null) {
-			    User root = newUserMap.get(rootId);
-			    userReportVo.setRootId(rootId);
-			    userReportVo.setRootRootName(root.getRootName());
-		    }
+			Long rootId = null;
+			if (user.getIsRoot() != null && user.getIsRoot()) {
+				rootId = userId;
+			} else {
+				tmpParentId = parentId;
+				whileTimes = 0;
+				while (tmpParentId != null) {
+					if (whileTimes > 1000) {
+						throw new BizException(BizCode.ERROR, "循环引用错误, user id is " + user.getId());
+					}
+					User parent = newUserMap.get(tmpParentId);
+					if (parent.getIsRoot() != null && parent.getIsRoot()) {
+						rootId = tmpParentId;
+						break;
+					}
+					tmpParentId = parent.getParentId();
+					whileTimes++;
+				}
+			}
+			if (rootId != null) {
+				User root = newUserMap.get(rootId);
+				userReportVo.setRootId(rootId);
+				userReportVo.setRootRootName(root.getRootName());
+			}
 			return userReportVo;
 
-	    }).collect(Collectors.toList());
+		}).collect(Collectors.toList());
 
-        logger.info("refresh end...");
+		logger.info("refresh end...");
 
-    }
+	}
 
-    @PostConstruct
-    public void init() {
-        refresh();
-    }
+	@PostConstruct
+	public void init() throws InterruptedException {
+		refresh();
+	}
+}
 
-
-
+class ThreadPool {
+	public final static ExecutorService executorService = Executors.newFixedThreadPool(12);
 
 }
+
