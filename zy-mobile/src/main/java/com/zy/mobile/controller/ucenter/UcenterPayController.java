212e4b0a8d65dbebae8996ad889d0984da30ce3a
Fred <fred_fuly@126.com>
2017-05-10 14:18:41 +0800

    !!

--- zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterPayController.java
+++ zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterPayController.java
@@ -92,13 +92,11 @@ public class UcenterPayController {
 
 	@RequestMapping
 	public String pay(@BigDecimalBinder BigDecimal money, @RequestParam PayType payType, @StringBinder String paymentSn, Model model, Principal principal) {
-
-		final BigDecimal zero = new BigDecimal("0.00");
-		String title = "积分余额充值";
-		Long userId = principal.getUserId();
-		validate(money, v -> v.compareTo(zero) > 0, "money must be more than 0.00");
-
 		if (StringUtils.isBlank(paymentSn)) {
+			final BigDecimal zero = new BigDecimal("0.00");
+			String title = "积分余额充值";
+			Long userId = principal.getUserId();
+			validate(money, v -> v.compareTo(zero) > 0, "money must be more than 0.00");
 			final BigDecimal amount = money;
 			DepositQueryModel depositQueryModel = new DepositQueryModel();
 			depositQueryModel.setUserIdEQ(principal.getUserId());
