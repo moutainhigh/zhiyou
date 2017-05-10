2e45476ae64425de14c9bb4ca0575899b17b45d9
Fred <fred_fuly@126.com>
2017-05-10 14:27:40 +0800

    !!

--- zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterPayController.java
+++ zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterPayController.java
@@ -175,7 +175,7 @@ public class UcenterPayController {
 		fuiouWeixinPayReq.setOpenid(userOpenId);
 		fuiouWeixinPayReq.setOutOrderNum(sn);
 		fuiouWeixinPayReq.setTranAmt(amount);
-		fuiouWeixinPayReq.setTerm_id("ft00001");
+		fuiouWeixinPayReq.setTerm_id("zy00001");
 		fuiouWeixinPayReq.setRemark(remark);
 		return fuiouWeixinPayReq;
 	}
