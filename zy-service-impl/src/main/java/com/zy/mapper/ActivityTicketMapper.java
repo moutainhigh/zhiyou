795a52ef98ddb78281103d6ef15632dbf320ea6e
Administrator <11@qq.com>
2017-05-25 10:25:04 +0800

    票

---
+++ zy-service-impl/src/main/java/com/zy/mapper/ActivityTicketMapper.java
@@ -0,0 +1,28 @@
+package com.zy.mapper;
+
+
+import com.zy.entity.act.ActivityTeamApply;
+import com.zy.entity.act.ActivityTicket;
+import com.zy.model.query.ActivityTeamApplyQueryModel;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
+public interface ActivityTicketMapper {
+
+	int insert (ActivityTicket activityTicket);
+
+	int update(ActivityTicket activityTicket);
+
+	int delete(Long id);
+
+	ActivityTicket findOne(Long id);
+
+//	List<ActivityTicket> findAll(ActivityTeamApplyQueryModel activityTeamApplyQueryModel);
+//
+//	long count(ActivityTeamApplyQueryModel activityTeamApplyQueryModel);
+
+
+
+}
