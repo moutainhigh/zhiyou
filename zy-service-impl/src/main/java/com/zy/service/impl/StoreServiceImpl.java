48e772762caedfb56b4c12d9750837cbd4abf82d
it001 <huazhaochun1991@sina.cn>
2017-08-03 10:55:54 +0800

    虚拟库存service添加

---
+++ zy-service-impl/src/main/java/com/zy/service/impl/StoreServiceImpl.java
@@ -0,0 +1,40 @@
+package com.zy.service.impl;
+
+import com.zy.common.model.query.Page;
+import com.zy.entity.act.PolicyCode;
+import com.zy.entity.act.Report;
+import com.zy.entity.sys.ConfirmStatus;
+import com.zy.entity.tour.Sequence;
+import com.zy.entity.tour.Tour;
+import com.zy.entity.tour.TourTime;
+import com.zy.entity.tour.TourUser;
+import com.zy.entity.usr.UserInfo;
+import com.zy.mapper.*;
+import com.zy.model.query.TourQueryModel;
+import com.zy.model.query.TourTimeQueryModel;
+import com.zy.model.query.TourUserQueryModel;
+import com.zy.service.StoreService;
+import com.zy.service.TourService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.zy.common.util.ValidateUtils.NOT_NULL;
+import static com.zy.common.util.ValidateUtils.validate;
+
+/**
+ * Created by it001 on 2017/6/27.
+ */
+@Service
+@Validated
+public class StoreServiceImpl implements StoreService {
+
+
+}
---
+++ zy-service/src/main/java/com/zy/service/StoreService.java
@@ -0,0 +1,18 @@
+package com.zy.service;
+
+import com.zy.common.model.query.Page;
+import com.zy.entity.tour.Sequence;
+import com.zy.entity.tour.Tour;
+import com.zy.entity.tour.TourTime;
+import com.zy.entity.tour.TourUser;
+import com.zy.entity.usr.UserInfo;
+import com.zy.model.query.TourQueryModel;
+import com.zy.model.query.TourTimeQueryModel;
+import com.zy.model.query.TourUserQueryModel;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface StoreService {
+   
+}
