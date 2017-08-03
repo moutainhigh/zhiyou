20d3356d624684ccc390c367945db6fe58caa67b
it001 <huazhaochun1991@sina.cn>
2017-08-03 10:48:50 +0800

    虚拟库存基础搭建

---
+++ zy-service/src/main/java/com/zy/model/query/OrderStoreQueryModel.java
@@ -0,0 +1,113 @@
+package com.zy.model.query;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.io.Serializable;
+
+import io.gd.generator.api.query.Direction;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStoreQueryModel implements Serializable {
+
+	private Long userIdEQ;
+
+	private Long orderIdEQ;
+
+	private Integer typeEQ;
+
+	private Integer numberGTE;
+
+	private Integer numberLT;
+
+	private Integer beforeNumberGTE;
+
+	private Integer beforeNumberLT;
+
+	private Integer afterNumberGTE;
+
+	private Integer afterNumberLT;
+
+	private Date createDateGTE;
+
+	private Date createDateLT;
+
+	private Long createByEQ;
+
+	private Integer isEndEQ;
+
+	private Integer pageNumber;
+
+	private Integer pageSize;
+
+	private String orderBy;
+
+	private Direction direction;
+
+	public void setOrderBy(String orderBy) {
+		if (orderBy != null && !fieldNames.contains(orderBy)) {
+			throw new IllegalArgumentException("order by is invalid");
+		}
+		this.orderBy = orderBy;
+	}
+
+	public Long getOffset() {
+		if (pageNumber == null || pageSize == null) {
+			return null;
+		}
+		return ((long) pageNumber) * pageSize;
+	}
+
+	public String getOrderByAndDirection() {
+		if (orderBy == null) {
+			return null;
+		}
+		String orderByStr = camelToUnderline(orderBy);
+		String directionStr = direction == null ? "desc" : direction.toString().toLowerCase();
+		return orderByStr + " " + directionStr;
+	}
+
+	private String camelToUnderline(String param) {
+		if (param == null || "".equals(param.trim())) {
+			return "";
+		}
+		int len = param.length();
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++) {
+			char c = param.charAt(i);
+			if (Character.isUpperCase(c)) {
+				sb.append("_");
+				sb.append(Character.toLowerCase(c));
+			} else {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+
+	private static Set<String> fieldNames = new HashSet<>();
+
+	static {
+		fieldNames.add("number");
+		fieldNames.add("afterNumber");
+		fieldNames.add("createBy");
+		fieldNames.add("orderId");
+		fieldNames.add("id");
+		fieldNames.add("type");
+		fieldNames.add("beforeNumber");
+		fieldNames.add("userId");
+		fieldNames.add("isEnd");
+		fieldNames.add("createDate");
+	}
+
+}
