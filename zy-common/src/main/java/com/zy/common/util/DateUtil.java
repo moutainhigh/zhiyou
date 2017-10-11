66ad9f373f4b4cb52fdb88d2cf13bf1138287049
Administrator <11@qq.com>
2017-10-11 10:06:46 +0800

    销量报表

--- zy-common/src/main/java/com/zy/common/util/DateUtil.java
+++ zy-common/src/main/java/com/zy/common/util/DateUtil.java
@@ -524,8 +524,8 @@ public class DateUtil {
         System.out.println("----------##########--");
         System.out.println(DateFormatUtils.format( DateUtil.getDateEnd(DateUtil.getMonthData(new Date(),0,-1)),"yyyy-MM-dd HH:mm:ss"));
         System.out.println(DateFormatUtils.format(DateUtil.getMothNum(DateUtil.getBeforeMonthBegin(new Date(),-2,0)),"yyyy-MM-dd HH:mm:ss"));
-        System.out.println("-------------------");
-        System.out.println(DateUtil.getDay(DateUtil.getMonthData(new Date(),0,-1)));
+        System.out.println("--------00000000-----------");
+        System.out.println(DateFormatUtils.format(DateUtil.getMonthData(new Date(),1,0),"yyyy-MM-dd HH:mm:ss"));
         System.out.println("---------1----------");
         System.out.println(DateFormatUtils.format(DateUtil.getCurrYearFirst(),"yyyy-MM-dd HH:mm:ss"));
         System.out.println("-------------------");
