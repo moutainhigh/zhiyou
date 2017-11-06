78b4086d3e4b0782a6e4cd815f2f23033f4d1d69
it001 <huazhaochun1991@sina.cn>
2017-11-06 09:55:30 +0800

    账号晋升逻辑

--- zy-service-impl/src/main/java/com/zy/service/impl/UserCheckServiceImpl.java
+++ zy-service-impl/src/main/java/com/zy/service/impl/UserCheckServiceImpl.java
@@ -2,15 +2,24 @@ package com.zy.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.zy.common.exception.BizException;
+import com.zy.common.util.DateUtil;
+import com.zy.entity.mal.Order;
 import com.zy.entity.mergeusr.MergeUser;
+import com.zy.entity.mergeusr.MergeUserUpgrade;
 import com.zy.entity.usr.User;
 import com.zy.mapper.MergeUserMapper;
+import com.zy.mapper.MergeUserUpgradeMapper;
+import com.zy.mapper.OrderMapper;
 import com.zy.model.BizCode;
 import com.zy.model.query.MergeUserQueryModel;
+import com.zy.model.query.MergeUserUpgradeQueryModel;
+import com.zy.model.query.OrderQueryModel;
 import com.zy.service.UserCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import java.util.function.Predicate;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +36,12 @@ public class UserCheckServiceImpl implements UserCheckService {
 
     @Autowired
     private MergeUserMapper mergeUserMapper;
+
+    @Autowired
+    private MergeUserUpgradeMapper mergeUserUpgradeMapper;
+
+    @Autowired
+    private OrderMapper orderMapper;
     /**
      * 处理用户关系 回调
      * @param id  购买人id
@@ -67,45 +82,90 @@ public class UserCheckServiceImpl implements UserCheckService {
      * @param quantity
      */
     private void disposeV0(MergeUser mergeUser,Long quantity,Integer prodectType){
+        MergeUserUpgrade mergeUserUpgrade = new MergeUserUpgrade();
+        mergeUserUpgrade.setFromUserRank(mergeUser.getUserRank());
+        mergeUserUpgrade.setUserId(mergeUser.getUserId());
+        mergeUserUpgrade.setUpgradedTime(new Date());
+        Map<String,Object> map = new HashMap<>();
+        map.put("productType" , prodectType);
+        map.put("userId" , mergeUser.getInviterId());
+        MergeUser parentInvter =  mergeUserMapper.findByUserIdAndProductType(map);
         //VIP
-       if(quantity>=4&&quantity<19){
-           mergeUser.setUserRank(User.UserRank.V1);
-           MergeUser parent = this.findParent(mergeUser,prodectType,2);
-               if(parent!=null){
-                   mergeUser.setParentId(parent.getUserId());
-                   mergeUserMapper.update(mergeUser);
-               }else{
-                   throw new BizException(BizCode.ERROR, "数据异常");
-               }
-       }
+           if(quantity>=4&&quantity<19){
+               mergeUser.setUserRank(User.UserRank.V1);
+               MergeUser parent = this.findParent(mergeUser,prodectType,2);
+                   if(parent!=null){
+                       if(parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V1){
+                           this.v1ToV2(parentInvter,prodectType);
+                       }
+                       mergeUser.setParentId(parent.getUserId());
+                       mergeUserMapper.update(mergeUser);
+                       mergeUserUpgrade.setToUserRank(User.UserRank.V1);
+                       //填入晋级信息
+                       mergeUserUpgradeMapper.insert(mergeUserUpgrade);
+                   }else{
+                       throw new BizException(BizCode.ERROR, "数据异常");
+                   }
+           }
         //品牌经理
-        if (quantity>=20&&quantity<149){
-            mergeUser.setUserRank(User.UserRank.V2);
-            MergeUser parent = this.findParent(mergeUser,prodectType,3);
-            if(parent!=null){
-                mergeUser.setParentId(parent.getUserId());
-                mergeUserMapper.update(mergeUser);
-            }else{
-                throw new BizException(BizCode.ERROR, "数据异常");
-            }
-            //处理 推荐人 晋升
-            Map<String,Object> map = new HashMap<>();
-            map.put("productType" , prodectType);
-            map.put("userId" , mergeUser.getInviterId());
-            MergeUser parentInvter =  mergeUserMapper.findByUserIdAndProductType(map);
-            if(parentInvter!=null&&parentInvter.getUserRank()!=null){
-                if (parentInvter.getUserRank()== User.UserRank.V1){ //vip 则看满足晋升不
-                    List<MergeUser> mergeUserList = mergeUserMapper.findAll(MergeUserQueryModel.builder().inviterIdEQ(parentInvter.getUserId()).build());
-                    if (mergeUserList==null){
-                        mergeUserList = mergeUserList.stream().filter(v->(v.getUserRank()==User.UserRank.V1
-                        ||v.getUserRank()==User.UserRank.V2||v.getUserRank()==User.UserRank.V3||v.getUserRank()==User.UserRank.V4)).collect(Collectors.toList());
-                        if(mergeUserList.size()+1>=5){//晋级
-
-                        }
-                    }
+            if (quantity>=20&&quantity<149){
+                mergeUser.setUserRank(User.UserRank.V2);
+                MergeUser parent = this.findParent(mergeUser,prodectType,3);
+                if(parent!=null){
+                    mergeUser.setParentId(parent.getUserId());
+                }else{
+                    throw new BizException(BizCode.ERROR, "数据异常");
                 }
+                //处理 推荐人 晋升
+                if(parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V1){
+                    this.v1ToV2(mergeUser,prodectType);
+                }else if(parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V2){
+                    this.v2ToV3(mergeUser,prodectType);
+                }
+                mergeUserMapper.update(mergeUser);  //让购买者升级
+                mergeUserUpgrade.setToUserRank(User.UserRank.V2);
+                //填入晋级信息
+                mergeUserUpgradeMapper.insert(mergeUserUpgrade);
             }
+        //品牌合伙人
+        if (quantity>=150&&quantity<2000){
+            mergeUser.setUserRank(User.UserRank.V3);
+            MergeUser parent = this.findParent(mergeUser,prodectType,3);
+            mergeUser.setParentId(parent.getUserId());
+            //处理 推荐人 晋升
+            if(parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V1){
+                this.v1ToV2(mergeUser,prodectType);
+            }else if (parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V2){
+                this.v2ToV3(mergeUser,prodectType);
+            }else if (parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V3){
+                this.v3ToV4(mergeUser,prodectType);
+            }
+            mergeUserMapper.update(mergeUser);  //让购买者升级
+            mergeUserUpgrade.setToUserRank(User.UserRank.V3);
+            //填入晋级信息
+            mergeUserUpgradeMapper.insert(mergeUserUpgrade);
         }
+        //处理
+        if (quantity>=2000){
+            //处理 推荐人 晋升
+            if(parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V1){
+                this.v1ToV2(mergeUser,prodectType);
+            }else if (parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V2){
+                this.v2ToV3(mergeUser,prodectType);
+            }else if (parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V3){
+                this.v3ToV4(mergeUser,prodectType);
+            }else if (parentInvter!=null&&parentInvter.getUserRank()== User.UserRank.V4){
+
+            }
+
+            mergeUser.setUserRank(User.UserRank.V3);
+            mergeUserMapper.update(mergeUser);  //让购买者升级
+            mergeUserUpgrade.setToUserRank(User.UserRank.V3);
+            //填入晋级信息
+            mergeUserUpgradeMapper.insert(mergeUserUpgrade);
+        }
+
+
     }
 
     /**
@@ -128,5 +188,218 @@ public class UserCheckServiceImpl implements UserCheckService {
       return parent;
     }
 
+    /**
+     * 统计 推荐人 大于等于当前等级的 新晋人数
+     * @param mergeUser
+     * @param prodectType
+     * @param num
+     * String type 1：代表推荐人 2：parentId 是这个和人的
+     * @return
+     */
+ private  Integer count(MergeUser mergeUser,Integer prodectType,int num,Integer type){
+     Predicate<MergeUser> predicate = v -> {
+         User.UserRank userRank = v.getUserRank();
+         if (num==0){
+             if (userRank != null && userRank == User.UserRank.V4|| userRank == User.UserRank.V3|| userRank == User.UserRank.V2|| userRank == User.UserRank.V1|| userRank == User.UserRank.V0) {
+                 return true;
+             }
+         }
+         if (num==1){
+             if (userRank != null && userRank == User.UserRank.V4|| userRank == User.UserRank.V3|| userRank == User.UserRank.V2|| userRank == User.UserRank.V1) {
+                 return true;
+             }
+         }
+         if (num==2){
+             if (userRank != null && userRank == User.UserRank.V4|| userRank == User.UserRank.V3|| userRank == User.UserRank.V2) {
+                 return true;
+             }
+         }
+         if (num==3){
+             if (userRank != null && userRank == User.UserRank.V4|| userRank == User.UserRank.V3) {
+                 return true;
+             }
+         }
+         if (num==4) {
+             if (userRank != null && userRank == User.UserRank.V4) {
+                 return true;
+             }
+         }
+         return false;
+     };
+     List<MergeUser> mergeUserList = null;
+     if(1==type) {
+         mergeUserList = mergeUserMapper.findAll(MergeUserQueryModel.builder().inviterIdEQ(mergeUser.getUserId())
+                 .productTypeEQ(prodectType).build()).stream().filter(predicate).collect(Collectors.toList());
+     }else{
+         mergeUserList = mergeUserMapper.findAll(MergeUserQueryModel.builder().parentIdEQ(mergeUser.getUserId())
+                 .productTypeEQ(prodectType).build()).stream().filter(predicate).collect(Collectors.toList());
+     }
+    Map<Long,List<MergeUserUpgrade>> map = mergeUserUpgradeMapper.findAll(MergeUserUpgradeQueryModel.builder().upgradedTimeGTE(DateUtil.getMonthData(new Date(),-2,0)).build())
+             .stream().collect(Collectors.groupingBy(MergeUserUpgrade::getUserId));
+     mergeUserList = mergeUserList.stream().filter(v->{
+         List<MergeUserUpgrade> mergeUserUpgradeList= map.get(v.getUserId());
+         if(mergeUserUpgradeList!=null&&!mergeUserUpgradeList.isEmpty()){
+              Map<User.UserRank,List<MergeUserUpgrade>> map1 = mergeUserUpgradeList.stream().collect(Collectors.groupingBy(MergeUserUpgrade::getToUserRank));
+             List<MergeUserUpgrade> list = map1.get(v.getUserRank());
+             if (list!=null&&!list.isEmpty()){
+                 return true;
+             }
+             return false;
+         }
+         return false;
+     }).collect(Collectors.toList());
+     return mergeUserList.size();
+ }
+
+
+    /**
+     * V1 晋升V2  逻辑
+     * @param parentInvter
+     * @param prodectType
+     */
+    private void v1ToV2(MergeUser parentInvter,Integer prodectType){
+            MergeUserUpgrade parentInvterUpgrade = new MergeUserUpgrade();
+            parentInvterUpgrade.setUserId(parentInvter.getUserId());
+            parentInvterUpgrade.setFromUserRank(parentInvter.getUserRank());
+            parentInvterUpgrade.setUpgradedTime(new Date());
+             int number = this.count(parentInvter,prodectType,1,1);
+             if(number+1>=5){ //让推荐人晋级
+                    parentInvterUpgrade.setToUserRank(User.UserRank.V2);
+                    parentInvter.setUserRank(User.UserRank.V2);
+                    MergeUser parent1 = this.findParent(parentInvter,prodectType,2);
+                    if(parent1!=null){
+                        parentInvter.setParentId(parent1.getUserId());
+                        mergeUserMapper.update(parentInvter);//更新推荐人
+                        //填入晋级信息
+                        mergeUserUpgradeMapper.insert(parentInvterUpgrade);
+                    }
+                }else{//看销量
+                  List<Order> orderList = orderMapper.findAll(OrderQueryModel.builder().productTypeEQ(prodectType).userIdEQ(parentInvter.getUserId())
+                          .createdTimeGTE(DateUtil.getMonthData(new Date(),-2,0)).build());
+                   if(orderList!=null&&!orderList.isEmpty()){
+                       Long sum = orderList.parallelStream().mapToLong(Order::getQuantity).sum();
+                       if (sum>=20&&sum<150){
+                           parentInvterUpgrade.setToUserRank(User.UserRank.V2);
+                           parentInvter.setUserRank(User.UserRank.V2);
+                           MergeUser parent1 = this.findParent(parentInvter,prodectType,2);
+                           if(parent1!=null){
+                               parentInvter.setParentId(parent1.getUserId());
+                               mergeUserMapper.update(parentInvter);//更新推荐人
+                               //填入晋级信息
+                               mergeUserUpgradeMapper.insert(parentInvterUpgrade);
+                           }
+                       }else if(sum>=150){
+                           parentInvterUpgrade.setToUserRank(User.UserRank.V3);
+                           parentInvter.setUserRank(User.UserRank.V3);
+                           MergeUser parent1 = this.findParent(parentInvter,prodectType,2);
+                           if(parent1!=null){
+                               parentInvter.setParentId(parent1.getUserId());
+                               mergeUserMapper.update(parentInvter);//更新推荐人
+                               //填入晋级信息
+                               mergeUserUpgradeMapper.insert(parentInvterUpgrade);
+                           }
+                       }
+                   }
+                }
+    }
+
+
+    /**
+     * v2升v3逻辑
+     */
+    private  void v2ToV3(MergeUser parentInvter,Integer prodectType){
+        MergeUserUpgrade parentInvterUpgrade = new MergeUserUpgrade();
+        parentInvterUpgrade.setUserId(parentInvter.getUserId());
+        parentInvterUpgrade.setFromUserRank(parentInvter.getUserRank());
+        parentInvterUpgrade.setUpgradedTime(new Date());
+        int number = this.count(parentInvter,prodectType,2,1);
+        if(number+1>=5){ //让推荐人晋级
+            parentInvterUpgrade.setToUserRank(User.UserRank.V3);
+            parentInvter.setUserRank(User.UserRank.V3);
+                mergeUserMapper.update(parentInvter);//更新推荐人
+                //填入晋级信息
+                mergeUserUpgradeMapper.insert(parentInvterUpgrade);
+        }else{//看销量
+            List<Order> orderList = orderMapper.findAll(OrderQueryModel.builder().productTypeEQ(prodectType).userIdEQ(parentInvter.getUserId())
+                    .createdTimeGTE(DateUtil.getMonthData(new Date(),-2,0)).build());
+            if(orderList!=null&&!orderList.isEmpty()){
+                Long sum = orderList.parallelStream().mapToLong(Order::getQuantity).sum();
+            if(sum>=150){
+                    parentInvterUpgrade.setToUserRank(User.UserRank.V3);
+                    parentInvter.setUserRank(User.UserRank.V3);
+                    mergeUserMapper.update(parentInvter);//更新推荐人
+                        //填入晋级信息
+                    mergeUserUpgradeMapper.insert(parentInvterUpgrade);
+                    }
+                }
+            }
+        }
+
+    /**
+     * V3晋升V4机制
+     * @param parentInvter
+     * @param prodectType
+     */
+    private void v3ToV4(MergeUser parentInvter,Integer prodectType){
+        MergeUserUpgrade parentInvterUpgrade = new MergeUserUpgrade();
+        parentInvterUpgrade.setUserId(parentInvter.getUserId());
+        parentInvterUpgrade.setFromUserRank(parentInvter.getUserRank());
+        parentInvterUpgrade.setUpgradedTime(new Date());
+        int number = this.count(parentInvter,prodectType,2,1);
+        if(number+1>=5) { //让推荐人晋级
+           Map<Long,List<MergeUser>> mergeUserMap = mergeUserMapper.findAll(MergeUserQueryModel.builder().productTypeEQ(prodectType).build()).stream()
+                    .collect(Collectors.groupingBy(MergeUser::getParentId));
+            Map<Long,List<Order>> orderMap = orderMapper.findAll(OrderQueryModel.builder().productTypeEQ(prodectType)
+                    .createdTimeGTE(DateUtil.getMonthData(new Date(),-2,0)).build()).stream().collect(Collectors.groupingBy(Order::getUserId));
+            long  num =0;
+            List<Order>myOrderLlist = orderMap.get(parentInvter.getUserId());
+            if (myOrderLlist!=null&&!myOrderLlist.isEmpty()){ //只计算已经是V3的量
+                num = myOrderLlist.stream().filter(v->v.getBuyerUserRank()== User.UserRank.V3).mapToLong(Order::getQuantity).sum();
+            }
+            for (MergeUser mergeUser:mergeUserMap.get(parentInvter.getUserId())){
+                if (mergeUser.getUserRank()== User.UserRank.V4){
+                    num +=150;
+                }else{
+                 List<Order> orderList = orderMap.get(mergeUser.getId());
+                    if(orderList!=null&&!orderList.isEmpty()){
+                        Long sum = orderList.parallelStream().filter(v->v.getBuyerUserRank()== mergeUser.getUserRank()).mapToLong(Order::getQuantity).sum();
+                        num+=sum;
+                        if(mergeUserMap.get(mergeUser.getUserId())!=null){
+                            for (MergeUser mergeUser1:mergeUserMap.get(mergeUser.getUserId())){
+                                if (mergeUser1.getUserRank()== User.UserRank.V4){
+                                    num +=150;
+                                }else{
+                                    List<Order> orderList1 = orderMap.get(mergeUser.getId());
+                                    if(orderList1!=null&&!orderList1.isEmpty()){
+                                        Long sum1 = orderList1.parallelStream().filter(v->v.getBuyerUserRank()== mergeUser1.getUserRank()).mapToLong(Order::getQuantity).sum();
+                                        num+=sum1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            if(num>=1800){
+                parentInvterUpgrade.setToUserRank(User.UserRank.V4);
+                parentInvter.setUserRank(User.UserRank.V4);
+                mergeUserMapper.update(parentInvter);//更新推荐人
+                //填入晋级信息
+                mergeUserUpgradeMapper.insert(parentInvterUpgrade);
+            }
+
+
+        }
+    }
+
+
+
+
+
 
 }
+
+
+
+
