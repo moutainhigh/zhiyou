d07f264446db951b2282168052810078317deec5
Administrator <xx@11.com>
2017-11-11 23:48:54 +0800

    过期时间修改

--- zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterNewOrderController.java
+++ zy-mobile/src/main/java/com/zy/mobile/controller/ucenter/UcenterNewOrderController.java
@@ -103,67 +103,79 @@ public class UcenterNewOrderController {
         }
         //判断团队人数
         if (userRank == User.UserRank.V3){
-            int whileTimes = 0;
-            while (parentId != null) {
-                if (whileTimes > 1000) {
-                    throw new BizException(BizCode.ERROR, "循环引用错误, user id is " + user.getId());
-                }
-                User parent = userService.findOne(parentId);
-                if (parent.getUserRank() == User.UserRank.V4) {
-                    parentId = parent.getId();
-                   break;
-                }else {
-                    parentId = parent.getParentId();
-                }
-                whileTimes ++;
-            }
-            //根据id查询团队省级人数
-            UserQueryModel userQueryModel  = new UserQueryModel();
-            userQueryModel.setIsDeletedEQ(false);
-            userQueryModel.setIsFrozenEQ(false);
-            List<User> users = userService.findAll(userQueryModel);
-            List<User> children = TreeHelper.sortBreadth2(users, parentId.toString(), v -> {
-                TreeNode treeNode = new TreeNode();
-                treeNode.setId(v.getId().toString());
-                treeNode.setParentId(v.getParentId() == null ? null : v.getParentId().toString());
-                return treeNode;
-            });
-
-            List<User> list = children.stream().filter(v -> v.getUserRank() == User.UserRank.V3).collect(Collectors.toList());
-
-            //找出第一个特级是parentId
-            List<User> uses = new ArrayList<>();
-            for (User use: list) {
-                Long pId = this.calculateV4UserId(use);
-                if (pId != null && pId.toString().equals(parentId.toString())){
-                    uses.add(use);
-                }
-            }
-            if (uses.size() >= 8 ){
-                //判断时间小于11 11 23 59 59
+//            int whileTimes = 0;
+//            while (parentId != null) {
+//                if (whileTimes > 1000) {
+//                    throw new BizException(BizCode.ERROR, "循环引用错误, user id is " + user.getId());
+//                }
+//                User parent = userService.findOne(parentId);
+//                if (parent.getUserRank() == User.UserRank.V4) {
+//                    parentId = parent.getId();
+//                   break;
+//                }else {
+//                    parentId = parent.getParentId();
+//                }
+//                whileTimes ++;
+//            }
+//            //根据id查询团队省级人数
+//            UserQueryModel userQueryModel  = new UserQueryModel();
+//            userQueryModel.setIsDeletedEQ(false);
+//            userQueryModel.setIsFrozenEQ(false);
+//            List<User> users = userService.findAll(userQueryModel);
+//            List<User> children = TreeHelper.sortBreadth2(users, parentId.toString(), v -> {
+//                TreeNode treeNode = new TreeNode();
+//                treeNode.setId(v.getId().toString());
+//                treeNode.setParentId(v.getParentId() == null ? null : v.getParentId().toString());
+//                return treeNode;
+//            });
+//
+//            List<User> list = children.stream().filter(v -> v.getUserRank() == User.UserRank.V3).collect(Collectors.toList());
+//
+//            //找出第一个特级是parentId
+//            List<User> uses = new ArrayList<>();
+//            for (User use: list) {
+//                Long pId = this.calculateV4UserId(use);
+//                if (pId != null && pId.toString().equals(parentId.toString())){
+//                    uses.add(use);
+//                }
+//            }
+//            if (uses.size() >= 8 ){
+//                //判断时间小于11 11 23 59 59
+//                Date expiredTime = null;
+//                Date date = new Date();
+//                Calendar calendar = Calendar.getInstance();
+//                calendar.set(2017, 10, 11, 23, 59 ,59);
+//                expiredTime = calendar.getTime();
+//                if (date.getTime() < expiredTime.getTime()){
+//                    isUse = 1;
+//                }else {
+//                    isUse = 0;
+//                }
+//            }else if (uses.size() < 8  && uses.size() > 0){
+//                //判断时间小于11 31 23 59 59
+//                Date expiredTime = null;
+//                Date date = new Date();
+//                Calendar calendar = Calendar.getInstance();
+//                calendar.set(2017, 11, 31, 23, 59 ,59);
+//                expiredTime = calendar.getTime();
+//                if (date.getTime() < expiredTime.getTime()){
+//                    isUse = 1;
+//                }else {
+//                    isUse = 0;
+//                }
+//            }
+                //判断时间小于2017, 10, 13, 23, 59 ,59
                 Date expiredTime = null;
                 Date date = new Date();
                 Calendar calendar = Calendar.getInstance();
-                calendar.set(2017, 10, 11, 23, 59 ,59);
+                calendar.set(2017, 10, 13, 23, 59 ,59);
                 expiredTime = calendar.getTime();
                 if (date.getTime() < expiredTime.getTime()){
                     isUse = 1;
                 }else {
                     isUse = 0;
                 }
-            }else if (uses.size() < 8  && uses.size() > 0){
-                //判断时间小于11 31 23 59 59
-                Date expiredTime = null;
-                Date date = new Date();
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(2017, 11, 31, 23, 59 ,59);
-                expiredTime = calendar.getTime();
-                if (date.getTime() < expiredTime.getTime()){
-                    isUse = 1;
-                }else {
-                    isUse = 0;
-                }
-            }
+
         }
 
 
